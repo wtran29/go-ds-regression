7c5295e81acab302d1a4165aa5745d923e632e1a
William Tran <wtran29@gmail.com>
2026-02-26 15:07:00 -0800

    call the train model function to test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -14,19 +15,22 @@ func main() {
 
 	// set up a logger
 	rootPath, _ := os.Getwd()
+	dataLogger := slog.New(slog.Default().Handler())
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level:     slog.LevelDebug,
 		AddSource: true,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			switch a.Key {
+			case slog.TimeKey:
+				a.Value = slog.StringValue(time.Now().Format("2006/01/02 03:04:05PM"))
+
 			case slog.SourceKey:
 				source, _ := a.Value.Any().(*slog.Source)
 				if source != nil {
 					// source.File = path.Base(source.File)
 					source.File = strings.TrimPrefix(source.File, filepath.ToSlash(rootPath)+"/")
+
 				}
-			case slog.TimeKey:
-				a.Value = slog.StringValue(time.Now().Format("2006/01/02 03:04:05PM"))
 			}
 			return a
 		},
@@ -35,5 +39,8 @@ func main() {
 	logger.Info("Parsed command line flags:", "features", config.FeatureVars)
 
 	// Either load or train a model
-	
+	_, _, err := getOrTrainModel(config, dataLogger)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Model error: %v", err))
+	}
 }
